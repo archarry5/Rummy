5d3e48509a1b262d342b323d3e87234804f549c7
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-09 13:00:00 +0530

    added numOfSequences() method

--- Cards/src/Cards/Rummy.java
+++ Cards/src/Cards/Rummy.java
@@ -4,37 +4,26 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Rummy {
-	ArrayList<Cards> cardSequence[] = new ArrayList<>();
+	ArrayList<Cards> cardSequence = new ArrayList<>();
+	ArrayList<Cards> sequences;
+	ArrayList<Cards> triples;
 	int joker;
 
 	Rummy(int[] sequence) {
+		sequences = new ArrayList<Cards>();
+		triples = new ArrayList<Cards>();
 		for (int i = 0; i < sequence.length; i++) {
-			cardSequence[i] = new Cards(sequence[i]);
+			cardSequence.add(new Cards(sequence[i]));
 		}
-		joker = (int) Math.floor(Math.random() * 52);
+		joker = (int) (Math.random() * 52);
+		Collections.sort(cardSequence)
 	}
 
 	boolean compareSet(Cards card1, Cards card2, Cards card3) {
-	if((card1.getPip() == card2.getPip()) && (card2.getPip() == card3.getPip())) {
-	                
-		if (card1.getsuit() == card2.getsuit()) {
-			return false;
-		}
-	        if (card2.getsuit() == card3.getsuit()) {
-			return false;
-		}
-	        if (card1.getsuit() == card3.getsuit()) {
-			return false;
-		} 
-	
-		return true;
-
-	  }
-		return false;
-		
+		return (card1.getPip() == card2.getPip()) && (card2.getPip() == card3.getPip()) 
 	}
 	
-               void maintainSet() {
+        void maintainSet() {
 
 		for (int i = 0; i < 12; i++) {
 			for (int j = i+1; j < 11; j++) {
@@ -59,24 +48,27 @@ public class Rummy {
 	}
 
 	boolean issequencewithjoker(ArrayList<Cards> ar, Cards joker) {
-		/*
-		 * boolean ret = true;
-		 */
 		return true;
 	}
 
-	boolean isNaturalsequence(ArrayList<Cards> ar) {
-
-		for (int i = 0; i < ) {
-			currentCard c = ar.get(i);
-			for (int j = 0; j < ar.size(); j++) {
-				if (Math.abs(c.sequence - ar.get(i).sequence) ==  1) {
-					count++;
-					currentCard
+	public int numOfNaturalsequence() {
+		int lifeCount = 0;
+		boolean isInSeq = new boolean[cardSequence.length]; 
+		int i = 0;	
+		while (i < cardSequence.length - 2) {
+			if (cardSequence.get(i + 2) - cardSequence.get(i) == 2) {
+				lifeCount++;
+				sequences.add(cardSequence.get(i)));
+				sequences.add(cardSequence.get(i + 1)));	
+				sequences.add(cardSequence.get(i + 2)));			
+				i += 3;		
+				if (i != cardSequence.length && (cardSequence.get(i) - cardSequence.get(i - 1))) {
+					sequences.add(cardSequence.get(i)));				
+					i++;	
 				}
-			} 
-		}
-
+			} else {		
+				i++;
+			}
+		return lifeCount;
 	}
-
 }
